5b29f3bd14859cd12635088e3a84e3820e043742
kimestelle <137699667+kimestelle@users.noreply.github.com>
2024-06-30 22:00:55 -0400

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HomePage from './components/HomePage';
+import Block from './components/Block';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/block", element: <Block /> },
+  ]
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
